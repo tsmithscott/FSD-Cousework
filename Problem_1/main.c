e8fe774a7ca29ff857c810a6736e6df1819dfd3d
theo <theosmithscott@protonmail.com>
2021-01-08 19:39:35 +0000

    Fixed Problem 1 to have a limited size

--- Problem_1/main.c
+++ Problem_1/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int check_format(char *password);
 
 int main() {
-    char password[9];
+    char password[50];
 
     printf("Enter a password in the following format:\nL5e$e55e\n\n");
-    fgets(password, 9, stdin);
+    fgets(password, 50, stdin);
     if (check_format(password)==1) {
         printf("\nPassword is compliant with the format");
     }
@@ -19,17 +20,22 @@ int main() {
 }
 
 int check_format(char *password) {
-  	if (isupper(password[0])==1 &&
-      	isdigit(password[1])==1 &&
-      	islower(password[2])==1 &&
-      	ispunct(password[3])==1 &&
-      	islower(password[4])==1 &&
-      	isdigit(password[5])==1 &&
-      	isdigit(password[6])==1 &&
-      	islower(password[7])==1) {
-    	return 1;
+	if (strlen(password) == 9) {
+		if (isupper(password[0])==1 &&
+			isdigit(password[1])==1 &&
+			islower(password[2])==1 &&
+			ispunct(password[3])==1 &&
+			islower(password[4])==1 &&
+			isdigit(password[5])==1 &&
+			isdigit(password[6])==1 &&
+			islower(password[7])==1) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
 	}
 	else {
-    	return 0;
-  	}
+		return 0;
+	}
 }
